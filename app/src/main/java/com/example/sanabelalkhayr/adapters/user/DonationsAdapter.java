ee47faf89e558dcbbb3ddf5b9938deabeacb63dd
saadfattouh <saadfattoh22@gmail.com>
2022-01-28 20:40:45 +0200

    12th commit

---
+++ app/src/main/java/com/example/sanabelalkhayr/adapters/user/DonationsAdapter.java
@@ -0,0 +1,205 @@
+package com.example.sanabelalkhayr.adapters.user;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.media.Image;
+import android.text.style.IconMarginSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.bumptech.glide.Glide;
+import com.example.sanabelalkhayr.R;
+import com.example.sanabelalkhayr.api.Urls;
+import com.example.sanabelalkhayr.model.Donation;
+import com.example.sanabelalkhayr.utils.SharedPrefManager;
+import com.google.android.material.navigation.NavigationView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DonationsAdapter extends RecyclerView.Adapter<DonationsAdapter.ViewHolder>{
+
+
+    Context context;
+    private List<Donation> donations;
+    public NavController navController;
+
+
+    // RecyclerView recyclerView;
+    public DonationsAdapter(Context context, ArrayList<Donation> donations) {
+        this.context = context;
+        this.donations = donations;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View listItem= layoutInflater.inflate(R.layout.item_donation, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);
+
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Donation donation = donations.get(position);
+
+        holder.details.setText(donation.getDescription());
+
+        holder.title.setText(donation.getTitle());
+
+        holder.quantity.setText(String.valueOf(donation.getQuantity()));
+
+        if(donation.getImage() != null){
+            Glide.with(context)
+                    .load(donation.getImage())
+                    .into(holder.image);
+        }
+
+
+
+        holder.orderBtn.setOnClickListener(v -> {
+            LayoutInflater factory = LayoutInflater.from(context);
+            final View view = factory.inflate(R.layout.order_confirmation_dialog, null);
+            final AlertDialog orderConfirmationDialog = new AlertDialog.Builder(context).create();
+            orderConfirmationDialog.setView(view);
+
+            TextView yes = view.findViewById(R.id.yes_btn);
+            TextView no = view.findViewById(R.id.no_btn);
+
+
+            yes.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    orderItem(donation.getId());
+
+                    orderConfirmationDialog.dismiss();
+
+                }
+            });
+
+            no.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    orderConfirmationDialog.dismiss();
+                }
+            });
+            orderConfirmationDialog.show();
+        });
+
+        holder.itemView.setOnClickListener(v -> {
+            navController = Navigation.findNavController(holder.itemView);
+            navController.navigate(R.id.action_donationsFragment_to_donationDetailsFragment);
+        });
+
+
+
+
+
+    }
+
+    private void orderItem(int id) {
+
+        int userId = SharedPrefManager.getInstance(context).getUserId();
+
+        final ProgressDialog pDialog = new ProgressDialog(context);
+        pDialog.setMessage("Processing Please wait...");
+        pDialog.show();
+
+        String url = Urls.BASE_URL + "";
+
+        AndroidNetworking.post(url)
+                .addBodyParameter("user_id", String.valueOf(userId))
+                .addBodyParameter("donation_id", String.valueOf(id))
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                        pDialog.dismiss();
+
+                        try {
+                            //converting response to json object
+                            JSONObject obj = response;
+
+                            //if no error in response
+                            if (obj.getInt("status") == 1) {
+
+                                Toast.makeText(context, obj.getString("message"), Toast.LENGTH_SHORT).show();
+
+                            } else if(obj.getInt("status") == -1){
+                                Toast.makeText(context, obj.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        pDialog.dismiss();
+                        Toast.makeText(context, anError.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return donations.size();
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView details;
+        public TextView title;
+        public TextView quantity;
+        public ImageView image;
+        public Button orderBtn;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.details = itemView.findViewById(R.id.description);
+            this.title = itemView.findViewById(R.id.title);
+            this.image = itemView.findViewById(R.id.image);
+            this.quantity = itemView.findViewById(R.id.quantity);
+            this.orderBtn = itemView.findViewById(R.id.order);
+        }
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/sanabelalkhayr/fragments/user/DonationsFragment.java
+++ app/src/main/java/com/example/sanabelalkhayr/fragments/user/DonationsFragment.java
@@ -6,15 +6,27 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.sanabelalkhayr.R;
+import com.example.sanabelalkhayr.adapters.user.DonationsAdapter;
+import com.example.sanabelalkhayr.model.Donation;
+
+import java.util.ArrayList;
 
 
 public class DonationsFragment extends Fragment {
 
     Context ctx;
+
+    ArrayList donations;
+    RecyclerView mList;
+    DonationsAdapter mAdapter;
+
 
     @Override
     public void onAttach(Context context) {
@@ -27,9 +39,41 @@ public class DonationsFragment extends Fragment {
     }
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_donations, container, false);
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        mList = view.findViewById(R.id.rv);
+
+        donations = new ArrayList<Donation>(){{
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+            add(new Donation(1, "burger", "good burger", null, 7));
+
+        }};
+
+        mAdapter = new DonationsAdapter(getContext(), donations);
+        mList.setAdapter(mAdapter);
 
 
     }
