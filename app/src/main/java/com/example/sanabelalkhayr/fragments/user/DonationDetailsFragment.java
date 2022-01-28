9d1e11ce2db84b28e66a02af0592f968e1ca9418
saadfattouh <saadfattoh22@gmail.com>
2022-01-28 19:24:48 +0200

    11th commit

--- app/src/main/java/com/example/sanabelalkhayr/fragments/user/DonationDetailsFragment.java
+++ app/src/main/java/com/example/sanabelalkhayr/fragments/user/DonationDetailsFragment.java
@@ -1,4 +1,4 @@
-package com.example.sanabelalkhayr;
+package com.example.sanabelalkhayr.fragments.user;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.sanabelalkhayr.R;
 
 /**
  * A simple {@link Fragment} subclass.
